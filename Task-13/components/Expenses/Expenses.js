d0b970fb212c76ce1d0c0ceeaa24d185b517f02d
satyam Singh <55867907+satyamhacker@users.noreply.github.com>
2023-10-30 10:56:34 +0530

    Task-13
    Task-13

---
+++ Task-13/components/Expenses/Expenses.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {filteredExpenses.length === 0 ? (
+          <p>No expenses found for the selected year.</p>
+        ) : (
+
+          <div>
+          {filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
+          {filteredExpenses.length === 1 && (
+            <p>only one Expense , Add more expenses to your list.</p>
+          )}
+        </div>
+
+        )}
+      </Card>
+    </div>
+  );
+};
+export default Expenses;
